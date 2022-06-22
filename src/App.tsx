17bcbfb7f88912830663cdb44ea3abf9b912df49
Luiz Adamchuk Jr <adamchuk.junior@gmail.com>
2022-06-21 21:26:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { gql, useQuery } from "@apollo/client";
+
+import { Event } from "./pages/Event";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+  return <Event />;
+}
+
+export default App;
